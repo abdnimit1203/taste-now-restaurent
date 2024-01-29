038969cc429a5aab0db4e3aeee4631a523b0593e
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-01-30 04:54:42 +0600

    toggling navbar icon created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import "./Navbar.css"
 import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Navbar = () => {
+  const toggle = document.getElementById('toggle')
+  toggle.onclick= function(){
+    toggle.classList.toggle('active')
+  }
   const navlinks = (
     <>
       <NavLink
@@ -60,6 +64,7 @@ const Navbar = () => {
     <div className="navbar">
       <nav className="nav-items">
         <div className="nav-1">
+          <div id="toggle"></div>
           <h2 id="logo" className="primary logo">
             Taste now
           </h2>
@@ -70,7 +75,7 @@ const Navbar = () => {
             {navlinks}
             </div>
             <MdOutlineShoppingCart className="icons" />
-            <button className="button mid-btn ">Sign up</button>
+            <button className="button mid-btn hidden">Sign up</button>
         </div>
       </nav>
     </div>
