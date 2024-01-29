1cdac4039104be4d74850c3823875a1c2a9c44d5
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-01-29 23:18:08 +0600

    Get to know us section added

--- src/components/KnowUs/KnowUs.jsx
+++ src/components/KnowUs/KnowUs.jsx
@@ -1,7 +1,38 @@
 const KnowUs = () => {
   return (
     <div>
-      <h2>Get to know <span className="primary">us</span> </h2>
+      <h2>
+        Get to know <span className="primary">us</span>{" "}
+      </h2>
+      <div className="grid">
+        <div className="flex">
+          <img src="/public/assets/teamwork.jpeg" alt="teamwork" />
+          <div>
+            <h2>
+              <span className="primary">Teamwork</span> is the key to our sucess
+            </h2>
+            <p>
+              At TasteNow we make sure that every decision is determined by a
+              team of us. Everyone´s opinion matters, and as group, we are
+              stronger. We firmly belive that team work, makes the dream work.
+            </p>
+          </div>
+        </div>
+        <div className="flex">
+          <img src="/public/assets/environment.jpeg" alt="environment" />
+          <div>
+            <h2>
+              We are all in for the
+              <span className="primary"> enviroment</span>
+            </h2>
+            <p>
+              TasteNow donates $1 penny for every purchase made from the app to
+              an organization that takes care of our enviromet. We belive that
+              with everyone´s help, we can make a better world.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
