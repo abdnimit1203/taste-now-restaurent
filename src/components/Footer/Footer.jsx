444bca0ceb890aa97a4096f914b676ba50d17974
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-01-29 23:42:40 +0600

    Footer section added and designed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import "./Footer.css";
+import { RiFacebookBoxFill } from "react-icons/ri";
+import { ImInstagram } from "react-icons/im";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <footer className=''>
-            <div>
-                <p id='logo' className='logo'>Taste now</p>
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div>
+        <span id="logo" className="logo white">
+          Taste now
+        </span>
+        <div className="flex">
+          <RiFacebookBoxFill className="footer-icons white" />
+          <ImInstagram className="footer-icons white" />
+          <FaTwitter className="footer-icons white" />
+          <FaYoutube className="footer-icons white" />
+        </div>
+      </div>
+      <div>
+        <a href="#" className="white block">
+          Seslendirme ve Alt Jazz
+        </a>
+        <a href="#" className="white block">
+          Media Market
+        </a>
+        <a href="#" className="white block">
+          Gillie
+        </a>
+        <a href="#" className="white block">
+          Size Last
+        </a>
+      </div>
+      <div>
+        <a href="#" className="white block">Self Betimes</a>
+        <a href="#" className="white block">Yatırımcı İlişkileri</a>
+        <a href="#" className="white block">Basal Himmler</a>
+      </div>
+      <div>
+        <a href="#" className="white block">Yard Market</a>
+        <a href="#" className="white block">Is İmkanları</a>
+        <a href="#" className="white block">Car Tercihleri</a>
+      </div>
+      <div>
+        <a href="#" className="white block">Hedge Karla</a>
+        <a href="#" className="white block">Mullein Koşulları</a>
+        <a href="#" className="white block">Autumnal Bulgier</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
